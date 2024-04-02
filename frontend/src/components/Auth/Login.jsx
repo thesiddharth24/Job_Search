e768afd3bf712fcf6cdefacd62106072bd73a74b
thesiddharth24 <siddharth20it028@gmail.com>
2024-04-02 19:34:40 +0530

    done

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -37,72 +37,74 @@ const Login = () => {
     }
   };
   
-  if(!isAuthorized){
-    return <Navigate to={'/login'}/>
-  }
+  // if(!isAuthorized){
+  //   return <Navigate to={'/login'}/>
+  // }
 
 
 
 
   if(isAuthorized){
     return <Navigate to={'/'}/>
-  }
-  return (
-    <>
-      <section className="authPage">
-        <div className="container">
-          <div className="header">
-            <img src="/JobZeelogo.png" alt="logo" />
-            <h3>Login to your account</h3>
+  }else{
+    return (
+      <>
+        <section className="authPage">
+          <div className="container">
+            <div className="header">
+              <img src="/JobZeelogo.png" alt="logo" />
+              <h3>Login to your account</h3>
+            </div>
+            <form>
+              <div className="inputTag">
+                <label>Login As</label>
+                <div>
+                  <select value={role} onChange={(e) => setRole(e.target.value)}>
+                    <option value="">Select Role</option>
+                    <option value="Employer">Employer</option>
+                    <option value="Job Seeker">Job Seeker</option>
+                  </select>
+                  <FaRegUser />
+                </div>
+              </div>
+              <div className="inputTag">
+                <label>Email Address</label>
+                <div>
+                  <input
+                    type="email"
+                    placeholder="zk@gmail.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <MdOutlineMailOutline />
+                </div>
+              </div>
+              <div className="inputTag">
+                <label>Password</label>
+                <div>
+                  <input
+                    type="password"
+                    placeholder="Your Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <RiLock2Fill />
+                </div>
+              </div>
+              <button type="submit" onClick={handleLogin}>
+                Login
+              </button>
+              <Link to={"/register"}>Register Now</Link>
+            </form>
           </div>
-          <form>
-            <div className="inputTag">
-              <label>Login As</label>
-              <div>
-                <select value={role} onChange={(e) => setRole(e.target.value)}>
-                  <option value="">Select Role</option>
-                  <option value="Employer">Employer</option>
-                  <option value="Job Seeker">Job Seeker</option>
-                </select>
-                <FaRegUser />
-              </div>
-            </div>
-            <div className="inputTag">
-              <label>Email Address</label>
-              <div>
-                <input
-                  type="email"
-                  placeholder="zk@gmail.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <MdOutlineMailOutline />
-              </div>
-            </div>
-            <div className="inputTag">
-              <label>Password</label>
-              <div>
-                <input
-                  type="password"
-                  placeholder="Your Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <RiLock2Fill />
-              </div>
-            </div>
-            <button type="submit" onClick={handleLogin}>
-              Login
-            </button>
-            <Link to={"/register"}>Register Now</Link>
-          </form>
-        </div>
-        <div className="banner">
-          <img src="/login.png" alt="login" />
-        </div>
-      </section>
-    </>
-  );
+          <div className="banner">
+            <img src="/login.png" alt="login" />
+          </div>
+        </section>
+      </>
+    );
+  }
+  
 };
 
 export default Login;
